21440db832a9ccc6a87d569814893aae01821a2b
Laguna Du <lag945@gmail.com>
2021-10-09 18:45:53 +0800

    Added L0212_Word_Search_IICPP.

---
+++ L0212_Word_Search_IICPP/L0212_Word_Search_IICPP/L0212_Word_Search_IICPP.cpp
@@ -0,0 +1,93 @@
+﻿// L0212_Word_Search_IICPP.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
+    if (!word.size())
+        return true;
+    if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[0])
+        return false;
+    char c = board[i][j];
+    board[i][j] = '*';
+    string s = word.substr(1);
+    bool ret = dfs(board, i - 1, j, s) || dfs(board, i + 1, j, s) || dfs(board, i, j - 1, s) || dfs(board, i, j + 1, s);
+    board[i][j] = c;
+    return ret;
+}
+class Solution {
+public:
+    static vector<string> findWords(vector<vector<char>>& v, vector<string>& s) {
+        vector<string> ans;
+        for (auto& x : s) {
+            int p = 0;
+            reverse(x.begin(), x.end());
+            for (int i = 0; i < v.size(); i++) {
+                for (int j = 0; j < v[0].size(); j++) {
+                    if (dfs(v, i, j, x)) {
+                        p = 1; break;
+                    }
+                }
+                if (p)break;
+            }
+            if (p) {
+                reverse(x.begin(), x.end());
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+    vector<vector<char> > a;
+
+    vector<char> temp;
+    temp.push_back('o');
+    temp.push_back('a');
+    temp.push_back('a');
+    temp.push_back('n');
+    a.push_back(temp);
+    temp.clear();
+    temp.push_back('e');
+    temp.push_back('t');
+    temp.push_back('a');
+    temp.push_back('e');
+    a.push_back(temp);
+    temp.clear();
+    temp.push_back('i');
+    temp.push_back('h');
+    temp.push_back('k');
+    temp.push_back('r');
+    a.push_back(temp);
+    temp.clear();
+    temp.push_back('i');
+    temp.push_back('f');
+    temp.push_back('l');
+    temp.push_back('v');
+    a.push_back(temp);
+    vector<string> b;
+	b.push_back("oath");// , "pea", "eat", "rain"})
+    b.push_back("pea");
+    b.push_back("eat");
+    b.push_back("rain");
+    vector<string> ret  = Solution::findWords(a, b);
+
+}
+
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
---
+++ L0212_Word_Search_IICS/L0212_Word_Search_IICS/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L0212_Word_Search_IICS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[][] a = new char[4][];
+
+            a[0] = new char[] { 'o', 'a', 'a', 'n' };
+            a[1] = new char[] { 'e', 't', 'a', 'e' };
+            a[2] = new char[] { 'i', 'h', 'k', 'r' };
+            a[3] = new char[] { 'i', 'f', 'l', 'v' };
+            var ret = FindWords(a, new string[] { "oath", "pea", "eat", "rain" });
+
+
+            /*
+            char[][] a = new char[1][];
+
+            a[0] = new char[] { 'a' };
+            var ret = FindWords(a, new string[] { "ab" });
+            */
+
+
+            //char[][] a = new char[1][];
+
+            //a[0] = new char[] { 'a', 'a' };
+            //var ret = FindWords(a, new string[] { "aaa" });
+
+
+            /*
+            char[][] a = new char[3][];
+
+            a[0] = new char[] { 'a', 'b', 'c', 'e' };
+            a[1] = new char[] { 'x', 'x', 'c', 'd' };
+            a[2] = new char[] { 'x', 'x', 'b', 'a' };
+            var ret = FindWords(a, new string[] { "abc", "abcd" });
+            */
+
+        }
+
+        // 40/51, not all pass
+        public static IList<string> FindWords(char[][] board, string[] words)
+        {
+            List<string> ret = new List<string>();
+            bool[] arrpearWords = new bool[words.Length];
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    for (int k = 0; k < words.Length; k++)
+                    {
+                        if (arrpearWords[k])
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            if (i == 2 && j == 3 && k==1)
+                            {
+                                Console.WriteLine("in'");
+                            }
+
+                            //find 4 adjacent cell
+                            bool appear = false;
+                            appear = findWord(board, words[k], i, j);
+                            Console.WriteLine(string.Format("{0},{1},{2}={3}", i, j, k, appear));
+                            arrpearWords[k] = appear;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < arrpearWords.Length; i++)
+            {
+                if (arrpearWords[i])
+                {
+                    ret.Add(words[i]);
+                }
+            }
+
+            return ret;
+        }
+
+        public static bool findWord(char[][] board, string word, int i, int j, int index = 0)
+        {
+            bool ret = false;
+
+            if (i >= 0 && i < board.Length)
+            {
+                if (j >= 0 && j < board[i].Length)
+                {
+                    if (board[i][j] == word[index])
+                    {
+                        Console.WriteLine(i.ToString() + "," + j.ToString() + "=" + word + "[" + index.ToString() + "]");
+                        // find next
+                        int next = index + 1;
+                        char temp = board[i][j];//don't go back
+                        board[i][j] = '*';
+                        if (next < word.Length)
+                        {
+                            List<int[]> directions = new List<int[]>();
+                            directions.Add(new int[] { i - 1, j });
+                            directions.Add(new int[] { i + 1, j });
+                            directions.Add(new int[] { i, j - 1 });
+                            directions.Add(new int[] { i, j + 1 });
+                            for (int k = 0; k < directions.Count; k++)
+                            {
+                                int a = directions[k][0];
+                                int b = directions[k][1];
+                                if (a >= 0 && a < board.Length && b >= 0 && b < board[a].Length)
+                                {
+                                    if (board[a][b] != '*')
+                                    {
+                                        ret = ret || findWord(board, word, a, b, next);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            ret = true;
+                        }
+
+                        board[i][j] = temp;
+                    }
+                    else
+                    {
+                        ret = false;
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+    }
+}
