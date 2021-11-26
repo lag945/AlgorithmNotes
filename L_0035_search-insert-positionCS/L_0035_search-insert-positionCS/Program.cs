fc4fe0cc9333ec584b698cc0c2242ac3f740dbe2
Laguna Du <lag945@gmail.com>
2021-11-26 10:24:35 +0800

    Added L0035.

---
+++ L_0035_search-insert-positionCS/L_0035_search-insert-positionCS/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_0035_search_insert_positionCS
+{
+    //https://leetcode.com/problems/search-insert-position/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int ret = s.SearchInsert(new int[] { 1, 3, 5, 6 }, 7);
+            ret = s.SearchInsert2(new int[] { 1, 3, 5, 6 }, 4);
+        }
+
+        public class Solution
+        {
+            public int SearchInsert(int[] nums, int target)
+            {
+                //T:O(log(n))
+                return FindIndex(nums, 0, nums.Length-1, target);
+            }
+
+            private int FindIndex(int[] nums, int left, int right, int target)
+            {
+                int ret = 0;
+                if (left >= right)
+                {
+                    if (target > nums[left])
+                    {
+                        ret = left + 1;
+                    }
+                    else
+                    {
+                        ret = left;
+                    }
+                }
+                else
+                {
+                    int mid = (left + right) / 2;
+
+                    if (target > nums[mid])
+                    {
+                        return FindIndex(nums, mid + 1, right, target);
+                    }
+                    else
+                    {
+                        return FindIndex(nums, left, right - 1, target);
+                    }
+                }
+
+                return ret;
+            }
+
+            public int SearchInsert2(int[] nums, int target)
+            {
+                int ret = (new List<int>(nums).BinarySearch(target));
+                if (ret < 0)
+                {
+                    ret = ret * -1 - 1;
+                }
+                return ret;
+            }
+        }
+    }
+}
---
+++ L_0035_search-insert-positionCS/L_0035_search-insert-positionCS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("L_0035_search-insert-positionCS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L_0035_search-insert-positionCS")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42e4a54d-ba2f-4bc5-9171-49899e7102ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
