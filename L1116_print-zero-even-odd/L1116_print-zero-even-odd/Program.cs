cda05ae11e7c2328b90a6daf0ad6567111dbe2b7
Laguna Du <lag945@gmail.com>
2022-01-07 14:11:02 +0800

    L1116、L1117

---
+++ L1116_print-zero-even-odd/L1116_print-zero-even-odd/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace L1116_print_zero_even_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class ZeroEvenOdd
+        {
+            private int n;
+            AutoResetEvent mreZero = new AutoResetEvent(true);
+            AutoResetEvent mreEven = new AutoResetEvent(false);
+            AutoResetEvent mreOdd = new AutoResetEvent(false);
+
+            public ZeroEvenOdd(int n)
+            {
+                this.n = n;
+            }
+
+            // printNumber(x) outputs "x", where x is an integer.
+            public void Zero(Action<int> printNumber)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    mreZero.WaitOne();
+                    printNumber(0);
+                    if (i % 2 == 0)
+                    {
+                        mreEven.Set();
+                    }
+                    else
+                    {
+                        mreOdd.Set();
+                    }
+                }
+            }
+
+            public void Even(Action<int> printNumber)
+            {
+                for (int i = 2; i <= n; i += 2)
+                {
+                    mreEven.WaitOne();
+                    printNumber(i);
+                    mreZero.Set();
+                }
+            }
+
+            public void Odd(Action<int> printNumber)
+            {
+                for (int i = 1; i <= n; i += 2)
+                {
+                    mreOdd.WaitOne();
+                    printNumber(i);
+                    mreZero.Set();
+                }
+            }
+        }
+    }
+}
---
+++ L1117_building-h2o/L1117_building-h2o/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace L1117_building_h2o
+{
+    class Program
+    {
+        //https://leetcode.com/problems/building-h2o/
+
+        static H2O h2o = new H2O();
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(CallO);
+            Thread t2 = new Thread(CallO);
+            Thread t3 = new Thread(CallO);
+            Thread t4 = new Thread(CallH);
+            Thread t5 = new Thread(CallH);
+            Thread t6 = new Thread(CallH);
+            Thread t7 = new Thread(CallH);
+            Thread t8 = new Thread(CallH);
+            Thread t9 = new Thread(CallH);
+            t1.Start(h2o);
+            t2.Start(h2o);
+            t3.Start(h2o);
+            t4.Start(h2o);
+            t5.Start(h2o);
+            t6.Start(h2o);
+            t7.Start(h2o);
+            t8.Start(h2o);
+            t9.Start(h2o);
+            //"OHOOHOHHHHOHHOHHHH"
+        }
+
+        public static void CallH(object data)
+        {
+            ((H2O)data).Hydrogen(null);
+        }
+
+        public static void CallO(object data)
+        {
+            ((H2O)data).Oxygen(null);
+        }
+
+
+        public class H2O
+        {
+            AutoResetEvent mreH = new AutoResetEvent(true);//auto每次只通過一個
+            AutoResetEvent mreO = new AutoResetEvent(false);
+            int status = 0;
+
+            public H2O()
+            {
+
+            }
+
+            public void Hydrogen(Action releaseHydrogen)
+            {
+
+                mreH.WaitOne();
+                System.Diagnostics.Debug.WriteLine("Hydrogen");
+                // releaseHydrogen() outputs "H". Do not change or remove this line.
+                //releaseHydrogen();
+                status++;
+                if (status == 1)
+                {
+                    mreH.Set();
+                }
+                else if (status == 2)
+                {
+                    mreO.Set();
+                }
+                else
+                { 
+                }
+            }
+
+            public void Oxygen(Action releaseOxygen)
+            {
+                mreO.WaitOne();
+                System.Diagnostics.Debug.WriteLine("Oxygen");
+                // releaseOxygen() outputs "O". Do not change or remove this line.
+                //releaseOxygen();
+                status = 0;
+                mreO.Reset();
+                mreH.Set();
+            }
+        }
+
+
+    }
+}
